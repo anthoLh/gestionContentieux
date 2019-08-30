32848da3ebdfd02acd16de1b2cbdfd0af84b9b6f
anthoLh <antho92220@hotmail.fr>
2019-08-30 11:25:21 +0200

    add controlleur + config web.xml

--- src/com/inti/controller/AffaireController.java
+++ src/com/inti/controller/AffaireController.java
@@ -1,5 +1,49 @@
 package com.inti.controller;
 
-public class AffaireController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.entities.Affaire;
+import com.inti.service.interfaces.IAffaireService;
+
+@CrossOrigin("*")
+@RestController
+public class AffaireController {
+	@Autowired
+	IAffaireService affaireService;
+	
+	@RequestMapping(value="affaires", method = RequestMethod.GET)
+	public List<Affaire> findAll(){
+		return affaireService.findAll(Affaire.class);
+	}
+	@RequestMapping(value="affaires/{idAffaire}", method = RequestMethod.GET)
+	public Affaire findOne(@PathVariable("idAffaire") Long idAffaire) {
+		return affaireService.findOne(Affaire.class, idAffaire);
+	}
+	@RequestMapping(value="affaires", method = RequestMethod.POST)
+	public void saveAffaire(@RequestBody Affaire affaire) {
+		affaireService.save(affaire);
+	}
+	@RequestMapping(value="affaires/{idAffaire}", method = RequestMethod.DELETE)
+	public void deleteAffaire(@PathVariable("idAffaire") Long idAffaire) {
+		affaireService.remove(idAffaire);
+	}
+	@RequestMapping(value="affaires/{idAffaire}", method= RequestMethod.PUT)
+	public void updateAffaire(@PathVariable("idAffaire") Long idAffaire,@RequestBody Affaire affaire) {
+		Affaire currentAffaire = affaireService.findOne(Affaire.class, idAffaire);
+		currentAffaire.setDescriptionAffaire(affaire.getDescriptionAffaire());
+		currentAffaire.setListDocument(affaire.getListDocument());
+		currentAffaire.setListTache(affaire.getListTache());
+		currentAffaire.setReference(affaire.getReference());
+		currentAffaire.setStatut(affaire.getStatut());
+		currentAffaire.setTitre(affaire.getTitre());
+		affaireService.save(currentAffaire);	
+	}
 }
--- src/com/inti/controller/UtilisateurController.java
+++ src/com/inti/controller/UtilisateurController.java
@@ -1,5 +1,50 @@
 package com.inti.controller;
 
-public class UtilisateurController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.entities.Utilisateur;
+import com.inti.service.interfaces.IUtilisateurService;
+
+@CrossOrigin("*")
+@RestController
+public class UtilisateurController {
+	@Autowired
+	IUtilisateurService utilisateurService;
+	
+	@RequestMapping(value="utilisateurs", method = RequestMethod.GET)
+	public List<Utilisateur> findAll(){
+		return utilisateurService.findAll(Utilisateur.class);
+	}
+	@RequestMapping(value="utilisateurs/{idUtilisateur}", method = RequestMethod.GET)
+	public Utilisateur findOne(@PathVariable("idUtilisateur") Long idUtilisateur) {
+		return utilisateurService.findOne(Utilisateur.class, idUtilisateur);
+	}
+	@RequestMapping(value="utilisateurs", method = RequestMethod.POST)
+	public void saveUtilisateur(@RequestBody Utilisateur utilisateur) {
+		utilisateurService.save(utilisateur);
+	}
+	@RequestMapping(value="utilisateurs/{idUtilisateur}", method = RequestMethod.DELETE)
+	public void deleteUtilisateur(@PathVariable("idUtilisateur") Long idUtilisateur) {
+		utilisateurService.remove(idUtilisateur);
+	}
+	@RequestMapping(value="utilisateurs/{idUtilisateur}", method= RequestMethod.PUT)
+	public void updateUtilisateur(@PathVariable("idUtilisateur") Long idUtilisateur,@RequestBody Utilisateur utilisateur) {
+		Utilisateur currentUtilisateur = utilisateurService.findOne(Utilisateur.class, idUtilisateur);
+		currentUtilisateur.setEmail(utilisateur.getEmail());
+		currentUtilisateur.setListRole(utilisateur.getListRole());
+		currentUtilisateur.setListTache(utilisateur.getListTache());
+		currentUtilisateur.setNomUtilisateur(utilisateur.getNomUtilisateur());
+		currentUtilisateur.setPassword(utilisateur.getPassword());
+		currentUtilisateur.setPrenomUtilisateur(utilisateur.getPrenomUtilisateur());
+		currentUtilisateur.setUsername(utilisateur.getUsername());
+		utilisateurService.save(currentUtilisateur);	
+	}
 }
