954d435f4a460b134d99a06239dad43a4a54d1a7
anthoLh <antho92220@hotmail.fr>
2019-09-03 11:59:39 +0200

    mise en com de la security

--- src/com/inti/config/SecurityConfig.java
+++ src/com/inti/config/SecurityConfig.java
@@ -1,57 +1,57 @@
-package com.inti.config;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import com.inti.service.interfaces.IAppUserDetailsService;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-@Autowired
-	private IAppUserDetailsService userDetailsService;
-
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
-
-	protected void configure(final HttpSecurity http) throws Exception {
-        http
-          .csrf().disable()
-          .authorizeRequests()
-          	.antMatchers("/gestionContentieux/**").hasRole("ADMIN")
-          	.antMatchers("/phases/**").hasRole("COMPTABLE")
-          	.antMatchers("/login*").permitAll()
-          	.anyRequest().authenticated()
-          .and()
-          .cors().and()
-          .formLogin()
-          	.loginProcessingUrl("/login")
-          .and()
-          .logout()
-          	.logoutUrl("/logout")
-          	.deleteCookies("JSESSIONID").permitAll().and()
-          .httpBasic()
-  		  .and();
-    }
-	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-}
+//package com.inti.config;
+//
+//import java.util.Arrays;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import com.inti.service.interfaces.IAppUserDetailsService;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Autowired
+//	private IAppUserDetailsService userDetailsService;
+//
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(userDetailsService);
+//	}
+//
+//	protected void configure(final HttpSecurity http) throws Exception {
+//        http
+//          .csrf().disable()
+//          .authorizeRequests()
+//          	.antMatchers("/gestionContentieux/**").hasRole("ADMIN")
+//          	.antMatchers("/phases/**").hasRole("COMPTABLE")
+//          	.antMatchers("/login*").permitAll()
+//          	.anyRequest().authenticated()
+//          .and()
+//          .cors().and()
+//          .formLogin()
+//          	.loginProcessingUrl("/login")
+//          .and()
+//          .logout()
+//          	.logoutUrl("/logout")
+//          	.deleteCookies("JSESSIONID").permitAll().and()
+//          .httpBasic()
+//  		  .and();
+//    }
+//	@Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        final CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//}
